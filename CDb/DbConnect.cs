2a88334e2c4bdeee86ead786a66593765dcba6de
Winston James Kruger <WinstonK@awards.co.za>
2019-10-23 15:33:04 +0200

    read

--- CDb/DbConnect.cs
+++ CDb/DbConnect.cs
@@ -31,13 +31,12 @@ namespace CDb
                "server=localhost;user id=root;password=;database=csharpmysql;");
             connection.Open();
             //MessageBox.Show("Connection Open  !");
-            String sql = "SELECT * FROM csharpmysql.test_table";
-            MySqlCommand cmd = new MySqlCommand(sql, connection);
-            MySqlDataReader reader = cmd.ExecuteReader();
-            while (reader.Read())
-            {
-                MessageBox.Show(reader[0].ToString()+" "+reader[1].ToString()+" "+reader[2].ToString());
-            }
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM csharpmysql.test_table where id=@id";
+            command.Parameters.AddWithValue("@id", textBox3.Text);
+            command.ExecuteNonQuery();
+            MySqlDataReader reader = command.ExecuteReader();
+            MessageBox.Show(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString());
             connection.Close();
             
         }
@@ -67,5 +66,27 @@ namespace CDb
             //age
 
         }
-    }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var connection = new MySqlConnection(
+               "server=localhost;user id=root;password=;database=csharpmysql;");
+            connection.Open();
+            //MessageBox.Show("Connection Open  !");
+            String sql = "SELECT * FROM csharpmysql.test_table WHERE Id=@id";
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", textBox3.Text);
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                MessageBox.Show(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString());
+            }
+            connection.Close();
+        }
+    }//command.CommandText = "SELECT * FROM test_table where id=@id";
 }
